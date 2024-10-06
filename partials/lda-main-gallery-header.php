451bd2c6082a6720a80532f2772644dfa9b0d4d3
Disnel Rodriguez <disnelrr@gmail.com>
2024-10-06 15:42:38 +0000

    Update gallery header

--- partials/lda-main-gallery-header.php
+++ partials/lda-main-gallery-header.php
@@ -11,28 +11,26 @@
 
 ?>
 
-<main id="content">
-  <section class="info-section page-section large-padding-regular-padding main-gallery">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h2>
-            <?php echo get_field('title'); ?>
-          </h2>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <p>
-            <?php echo get_field('intro'); ?>
-          </p>
-        </div>
+<section class="info-section page-section large-padding-regular-padding main-gallery">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>
+          <?php echo get_field('title'); ?>
+        </h2>
       </div>
     </div>
-  </section>
+    <div class="row">
+      <div class="col-12">
+        <p>
+          <?php echo get_field('intro'); ?>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
 
-  <section>
-    <?php get_template_part('template_part/gallery-menu'); ?>
-    <div class="gallery-separator"></div>
-  </section>
-</main>
+<section>
+  <?php get_template_part('template_part/gallery-menu'); ?>
+  <div class="gallery-separator"></div>
+</section>
